17bc4cf13b9e65d7036b9427070d23878dc78e5a
Ismadero <ismaelmadero2003@gmail.com>
2024-09-02 16:28:58 -0300

    Corregi el string_to_array y arranque el proc run_cmd

--- execute.c
+++ execute.c
@@ -15,14 +15,21 @@ char *comm_inter[] = {
 
 unsigned int long_comm  = sizeof(comm_inter) / sizeof(comm_inter[0]);
 
-char *str[SIZE] command_to_array (scommand command) {
+char *command_to_array (scommand command) {
 	assert (command != NULL);
 	char *result[SIZE];
+	char *aux = NULL;
 	guint length = scommand_length(command);
 	for (guint i=0; i < length; i++) {
-		result[i] = g_queue_peek_nth(scommand->scomman, i);
+		aux = scommand_front(command);
+		result[i] = malloc(strlen(aux)+1);
+		strcpy(result[i], aux);
+		scommand_pop_front(command);
 	}
-	result[length] = NULL;
+	aux = scommand_get_redir_in(command);
+	result[length] = malloc(strlen(aux)+1);
+	strcpy(result[length], aux);
+	result[length+1] = NULL;
 	return result;
 }
 
@@ -40,10 +47,49 @@ bool is_command (const scommand cmd){
     return is_command;
 }
 
+void command_run (const scommand cmd, char *input, pipeline apipe) {
+    bool wait = pipeline_get_wait(apipe);
+	operator opp = scommand_get_operator(cmd);
+	pid_t pid;
+	if (input != NULL) {
+		command_set_redir_in(cmd,input);
+	}
+	if (opp == PIPELINE) { 			// Ve el caso de hacer un PIPE
+		int pipefd[2];				// la idea con esto es hacer una
+		if (pipe(pipefd) == -1) {	// llamada a command_run con el
+			perror("pipe");			// siguiente comando del pipeline
+			exit(EXIT_FAILURE);
+		}
+	}
+	pid=fork();
+	if (pid == -1) {
+		perror("fork");
+		exit(EXIT_FAILURE);
+	} else if (pid == 0) {
+		char *cmd_arr[SIZE] = command_to_array(cmd);
+		execvp(cmd_arr[0], cmd_arr);
+		if (opp == PIPELINE) {
+			cmd = scommand_destroy(cmd);
+        	cmd = pipeline_front(apipe);
+        	pipeline_pop_front(apipe);
+			// de alguna forma pasar el output del comando actual 
+			// en el apartado char *input en la llamada del sig
+			// command_run
+		}
+	}
+	//
+	//
+	//COMPLETAR
+	//Funcion open, close, y dup.
+	//
+	if (wait) {
+		wait(NULL);
+	}
+}
+
 void execute_pipeline(pipeline apipe){
     assert(apipe != NULL);
     scommand cmd;
-    bool wait = pipeline_get_wait(apipe);
     bool command=true, builtin=true;
     while (pipeline_length(apipe)!=0 && (command || builtin)) {
         cmd = pipeline_front(apipe);
@@ -53,12 +99,13 @@ void execute_pipeline(pipeline apipe){
         if ((!command && !builtin) || (command && builtin)) {
             printf("error\n");
         }
-        else if (command && !builtin) { //syscall
-            command_run(cmd);
-        }
         else if (builtin && !command){
             builtin_run(cmd);
         }
+        else if (command && !builtin) { //syscall
+			while (!pipeline_is_empty(apipe)) {
+				command_run(cmd, NULL, apipe);
+			}
+        }
     }
-    
 }
